c559bfae1874895d2ccbf2b92da4aec4f6d9da91
Robin Kuzmin <kuzmin.robin@gmail.com>
2024-12-04 12:06:35 -0800

    * TRPL.2024.11/08.Collections/03.HashMaps/src/main.rs

--- TRPL.2024.11/08.Collections/03.HashMaps/src/main.rs
+++ TRPL.2024.11/08.Collections/03.HashMaps/src/main.rs
@@ -1,32 +1,31 @@
-
 fn main() {
     // https://rust-book.cs.brown.edu/ch08-03-hash-maps.html#summary
-    // Given a list of integers, use a vector and return 
-    // the median (when sorted, the value in the middle position) 
-    // and mode (the value that occurs most often; a hash map will be helpful here) 
+    // Given a list of integers, use a vector and return
+    // the median (when sorted, the value in the middle position)
+    // and mode (the value that occurs most often; a hash map will be helpful here)
     // of the list.
     {
         fn median(s: &[i32]) -> Option<i32> {
-            let mut v = s.to_vec();
-            v.sort();
-            let len = v.len();
+            let len: usize = s.len();
             if len == 0 {
                 return None;
             }
+            let mut v: Vec<i32> = s.to_vec();
+            v.sort();
             Some(v[len / 2])
         }
         fn mode(s: &[i32]) -> Option<i32> {
             if s.len() == 0 {
-                return None
+                return None;
             }
             use std::collections::HashMap;
-            let mut hm = HashMap::new();
+            let mut hm: HashMap<i32, usize> = HashMap::new();   // maps the element value to count.
             for el in s {
-                let count = hm.entry(*el).or_insert(0_usize);   //.insert(el)
+                let count: &mut usize = hm.entry(*el).or_insert(0_usize);
                 *count += 1;
             }
-            let mut most_freq_el = s[0];
-            let mut highest_count = *hm.get(&most_freq_el).unwrap();
+            let mut most_freq_el: i32 = s[0];
+            let mut highest_count: usize = *hm.get(&most_freq_el).unwrap();
             for (k_el, v_count) in hm {
                 if v_count > highest_count {
                     highest_count = v_count;
@@ -43,6 +42,6 @@ fn main() {
         println!("most_often: {:?}", mode(&v));
         // sorted: [-6, -4, -4, 0, 1, 3, 3, 7, 7, 7, 9, 9]
         // median: Some(3)
-        // most_often: Some(7)        
+        // most_often: Some(7)
     }
 }
